2d591d37823f496bf29a3b3583862c797d8de783
Leon <leon390422914@gmail.com>
2019-10-16 13:32:44 +0800

    WIP

---
+++ include/eosblog.hpp
@@ -0,0 +1,68 @@
+#include <eosio/eosio.hpp>
+#include <eosio/singleton.hpp>
+
+using namespace std;
+using namespace eosio;
+
+CONTRACT eosblog : public contract {
+  public:
+    using contract::contract;
+
+    // general actions
+    ACTION setconfig(string blogname, string nickname, string version, string metadata);
+    ACTION login();
+    // posts actions
+    ACTION createpost(string title, string content, string cover, string author, name category, string metadata);
+    ACTION updatepost(uint64_t id, string title, string content, string cover, string author, name category, string metadata);
+    ACTION deletepost(uint64_t id);
+    // clear
+    ACTION clear();
+
+  private:
+    void _add_category(name category);
+    void _sub_category(name category);
+
+    TABLE config {
+      string  blogname;
+      string  nickname;
+      string  cover;
+      string  version;
+      string  metadata;
+    };
+    typedef singleton<"config"_n, config> config_index;
+
+    // published posts
+    TABLE post {
+      uint64_t  id;
+      string    title;
+      string    content;
+      string    cover;
+      string    author;
+      name      category;
+      uint32_t  created_at;
+      uint32_t  updated_at;
+      string    metadata;
+
+      uint64_t  primary_key() const { return id; }
+      uint64_t  get_catagory() const { return category.value; }
+    };
+    typedef multi_index<"post"_n, post,
+      indexed_by<"bycategory"_n, const_mem_fun<post, uint64_t, &post::get_catagory>>
+    > post_index;
+
+    TABLE category {
+      name      categoryname;
+      uint64_t  count;
+
+      uint64_t primary_key() const { return categoryname.value; }
+    };
+    typedef multi_index<"category"_n, category> category_index;
+    
+    TABLE tag {
+      name      tagname;
+      uint64_t  count;
+      
+      uint64_t primary_key() const { return tagname.value; }
+    };
+    typedef multi_index<"tag"_n, tag> tag_index;
+};
---
+++ src/eosblog.cpp
@@ -0,0 +1,167 @@
+#include <eosblog.hpp>
+
+ACTION eosblog::setconfig(string blogname, string nickname, string version, string metadata) {
+  require_auth(get_self());
+  
+  config_index config_table(get_self(), get_self().value);
+  auto config_singleton = config_table.get_or_create(get_self(), config { blogname, version, metadata });
+  // update config
+  config_singleton.blogname = blogname;
+  config_singleton.nickname = nickname;
+  config_singleton.version = version;
+  config_singleton.metadata = metadata;
+  // save config to table
+  config_table.set(config_singleton, get_self());
+}
+
+ACTION eosblog::login() {
+  require_auth(get_self());
+}
+
+ACTION eosblog::createpost(string title, string content, string cover, string author, name category, string metadata) {
+  require_auth(get_self());
+
+  // update post table
+  post_index post_table(get_self(), get_self().value);
+  post_table.emplace(get_self(), [&](auto& row) {
+    uint32_t timestamp = current_time_point().sec_since_epoch();
+    row.id = (uint64_t) timestamp;
+    row.title = title;
+    row.content = content;
+    row.cover = cover;
+    row.author = author;
+    row.category = category;
+    row.metadata = metadata;
+    row.created_at = timestamp;
+    row.updated_at = timestamp;
+  });
+
+  // update category table
+  _add_category(category);
+}
+
+ACTION eosblog::updatepost(uint64_t id, string title, string content, string cover, string author, name category, string metadata) {
+  require_auth(get_self());
+
+  // update post table
+  post_index post_table(get_self(), get_self().value);
+  auto post_itr = post_table.find(id);
+  // if the post exists
+  if (post_itr != post_table.end()) {
+    // preserve old category and sub it later
+    name old_category = post_itr->category;
+    // update post
+    post_table.modify(post_itr, get_self(), [&](auto& row) {
+      uint32_t timestamp = current_time_point().sec_since_epoch();
+      row.title = title;
+      row.content = content;
+      row.cover = cover;
+      row.author = author;
+      row.category = category;
+      row.metadata = metadata;
+      row.updated_at = timestamp;
+    });
+    // update category table
+    if (old_category != category) {
+      _sub_category(old_category);
+      _add_category(category);
+    }
+  }
+}
+
+ACTION eosblog::deletepost(uint64_t id) {
+  require_auth(get_self());
+
+  // update post table
+  post_index post_table(get_self(), get_self().value);
+  auto post_itr = post_table.find(id);
+  if (post_itr != post_table.end()) {
+    // preserve old category and sub it later
+    name old_category = post_itr->category;
+    post_itr = post_table.erase(post_itr);
+    // update category table
+    _sub_category(old_category);
+  }
+}
+
+ACTION eosblog::clear() {
+  require_auth(get_self());
+
+  // remove config table
+  config_index config_table(get_self(), get_self().value);
+  config_table.remove();
+}
+
+
+// Private
+void eosblog::_add_category(name category) {
+  category_index category_table(get_self(), get_self().value);
+  auto category_itr = category_table.find(category.value);
+  if (category_itr == category_table.end()) {
+    // does not exist
+    category_table.emplace(get_self(), [&](auto& row) {
+      row.categoryname = category;
+      row.count = 1;
+    });
+  } else {
+    // exists
+    category_table.modify(category_itr, get_self(), [&](auto& row) {
+      row.count = row.count + 1;
+    });
+  }
+}
+
+void eosblog::_sub_category(name category) {
+  category_index category_table(get_self(), get_self().value);
+  auto category_itr = category_table.find(category.value);
+  if (category_itr != category_table.end()) {
+    // if the category exists
+    if (category_itr->count <= 1) {
+      // delete category
+      category_itr = category_table.erase(category_itr);
+    } else {
+      // reduce counter
+      category_table.modify(category_itr, get_self(), [&](auto& row) {
+        row.count = row.count - 1;
+      });
+    }
+  }
+}
+
+/*
+ACTION eosblog::hi(name from, string message) {
+  require_auth(from);
+
+  // Init the _message table
+  messages_table _messages(get_self(), get_self().value);
+
+  // Find the record from _messages table
+  auto msg_itr = _messages.find(from.value);
+  if (msg_itr == _messages.end()) {
+    // Create a message record if it does not exist
+    _messages.emplace(from, [&](auto& msg) {
+      msg.user = from;
+      msg.text = message;
+    });
+  } else {
+    // Modify a message record if it exists
+    _messages.modify(msg_itr, from, [&](auto& msg) {
+      msg.text = message;
+    });
+  }
+}
+
+ACTION eosblog::clear() {
+  require_auth(get_self());
+
+  messages_table _messages(get_self(), get_self().value);
+
+  // Delete all records in _messages table
+  auto msg_itr = _messages.begin();
+  while (msg_itr != _messages.end()) {
+    msg_itr = _messages.erase(msg_itr);
+  }
+}
+*/
+
+EOSIO_DISPATCH(eosblog, (setconfig)(login)(clear)(createpost)(updatepost)(deletepost))
